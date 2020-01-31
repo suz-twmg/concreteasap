4792a2c710ac0b28c8d9e2a7e2f96f25533798a5
suz-twmg <sujan@thewebsitemarketinggroup.com.au>
2020-01-31 12:29:40 +1100

    ALL users

--- routes/api.php
+++ routes/api.php
@@ -26,8 +26,7 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 Route::get("test",function(App $app,OrderRepositoryInterface $orderRep,User $user,Order $order){
-    $user=User::where("email","Leadsconcrete@hotmail.com")->delete();
-    var_dump($user);
+    var_dump( User::all());
 
 });
 
