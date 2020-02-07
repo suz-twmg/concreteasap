78f4f362c1a9db60c4481b610571e7b75219bc13
suz-twmg <sujan@thewebsitemarketinggroup.com.au>
2020-02-07 16:51:34 +1100

    Update

--- app/Models/Order/Order.php
+++ app/Models/Order/Order.php
@@ -76,12 +76,7 @@ class Order extends Model
         if(!is_null($last_job)){
             $last_job_id=$last_job->job_id;
             $last_job_arr=explode("-",$last_job_id);
-            var_dump($last_job_arr);
-            if(isset($job_arr[1])&&isset($job_arr[2])){
-                var_dump($last_job_arr[1]===$month);
-                var_dump($last_job_arr[2]===$year);
-                var_dump($month);
-                var_dump($year);
+            if(isset($last_job_arr[1])&&isset($last_job_arr[2])){
                 if($last_job_arr[1]===$month&&$last_job_arr[2]===$year){
                     $id=((int)$last_job_arr[0])+1;
                     $job_id=$id."-".$month."-".$year;
