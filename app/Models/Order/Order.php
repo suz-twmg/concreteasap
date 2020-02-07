861084e7505a2ef31166c19fa49833f5bbae0d64
suz-twmg <sujan@thewebsitemarketinggroup.com.au>
2020-02-07 16:38:11 +1100

    Fix Iusse

--- app/Models/Order/Order.php
+++ app/Models/Order/Order.php
@@ -71,7 +71,7 @@ class Order extends Model
         $year=$current_date->format("y");
         $job_id="0-".$month."-".$year;
 
-        $last_job=$this->orderBy('id','desc')->first();
+        $last_job=Order::orderBy('id','desc')->first();
 
         if(!is_null($last_job)){
             $last_job_id=$last_job->job_id;
