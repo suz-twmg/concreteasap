44c5ab1f906b841e782a46164237e29d7df44231
suz-twmg <sujan@thewebsitemarketinggroup.com.au>
2020-02-11 17:38:18 +1100

    New

--- app/Repositories/OrderRepository.php
+++ app/Repositories/OrderRepository.php
@@ -345,25 +345,17 @@ class OrderRepository implements Interfaces\OrderRepositoryInterface
 
     public function getDayOfPourOrders()
     {
-        return $this->user->orders()->whereHas("orderConcrete")->whereHas("bids", function ($query) {
-            $query->where("date_delivery","=",\Illuminate\Support\Carbon::now('Australia/Sydney')->format("Y-m-d"));
-        })->with(["message","orderConcrete","user","bids" => function ($query) {
+        $orders = $this->user->orders()->with(["orderConcrete", "user", "bids" => function ($query) {
             $query->with(["user" => function ($query) {
                 $query->with(["detail" => function ($query) {
                     $query->select(["user_id", "company", "first_name", "last_name", "phone_number", "profile_image", "abn"]);
                 }])->select(["id", "email"]);
             }])->where("status", "Accepted");
-        }])->whereIn("status", ["Accepted", "Released", "Paid"])->get();
+        }])->whereHas("bids", function ($query) {
+            $query->where("date_delivery", \Illuminate\Support\Carbon::now('Australia/Sydney')->format("Y-m-d"));
+        })->whereIn("status", ["Accepted", "Released", "Paid"])->paginate(20);
 
-//        return $this->user->orders()->whereHas("orderConcrete")->whereHas("bids", function ($q) {
-//            return $q->where("date_delivery","!=", \Illuminate\Support\Carbon::now('Australia/Sydney')->format("Y-m-d"));
-//        })->with(["message", "orderConcrete", "bids" => function ($query) {
-//            $query->with(["user" => function ($query) {
-//                $query->with(["detail" => function ($query) {
-//                    $query->select(["user_id", "company", "first_name", "last_name", "phone_number", "profile_image", "abn"]);
-//                }])->select(["id", "email"]);
-//            }])->where("status", "Accepted");
-//        }])->whereIn("status", ["Accepted", "Released", "Paid"])->orderBy("id", "DESC")->get();
+        return $orders;
     }
 
     public function messageOrder(int $order_id, float $quantity)
