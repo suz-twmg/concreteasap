0f28651807a5b5867a6aff4ca8dfac1a919c69bd
suz-twmg <sujan@thewebsitemarketinggroup.com.au>
2020-03-02 17:27:42 +1100

    New Update

--- app/Http/Controllers/Contractor/OrderController.php
+++ app/Http/Controllers/Contractor/OrderController.php
@@ -308,8 +308,6 @@ class OrderController extends Controller
                     "bid_id" =>$accepted_bid["id"]
                 )
             ];
-            var_dump($accepted_bid["bid_id"]);
-            var_dump($accepted_bid->user);
             Notification::send($accepted_bid->user, new AppNotification($notification));
         }
         catch (\Exception $e) {
