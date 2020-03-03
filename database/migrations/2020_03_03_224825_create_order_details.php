b7cab0b893ca095076c211759c24a0f6b5c01d9d
suz-twmg <sujan@thewebsitemarketinggroup.com.au>
2020-03-04 10:33:16 +1100

    Reo Migration

--- database/migrations/2020_03_03_224825_create_order_details.php
+++ database/migrations/2020_03_03_224825_create_order_details.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateOrderReo extends Migration
+class CreateOrderDetails extends Migration
 {
     /**
      * Run the migrations.
