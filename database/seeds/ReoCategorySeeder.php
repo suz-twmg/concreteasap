705d969b10cff6ea9289cb4a95e75fdf63c1c15c
suz-twmg <sujan@thewebsitemarketinggroup.com.au>
2020-03-06 10:31:15 +1100

    Add Reo Seeder

---
+++ database/seeds/ReoCategorySeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class ReoCategorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+//        $reo_category=factory("App\Models\")->create([
+//
+//        ]);
+    }
+}
