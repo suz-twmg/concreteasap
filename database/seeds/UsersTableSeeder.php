a95ca9eae8c78693332d0032f6fee6421821abd3
suz-twmg <sujan@thewebsitemarketinggroup.com.au>
2020-03-06 10:24:34 +1100

    User Seeder

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -13,7 +13,7 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        //        
+        //
         $admin_role = factory("App\Role")->create([
         	'name'=>"administrator",
         	"description"=>"administrator"
@@ -24,17 +24,22 @@ class UsersTableSeeder extends Seeder
         	"description"=>"Rep Role"
         ]);
 
+        $reo_role=factory("App\Role")->create([
+            'name'=>"reo_role",
+            "description"=>"Reo Role"
+        ]);
+
         $contractor_role=factory("App\Role")->create([
         	'name'=>"contractor",
         	"description"=>"Contractor Role"
-        ]);        
+        ]);
 
         $admin_user = factory("App\User")->create([
         	'username'=>'reggie',
         	"device_id"=>"",
         	'status'=>'verified',
 	        'email'=>"reggie@twmg.com.au",
-	        "password"=>bcrypt("twmg#2019")		        
+	        "password"=>bcrypt("twmg#2019")
 	    ])->each(function($admin_user) use($admin_role){
 	    	$admin_user->roles()->sync($admin_role);
 	    });
@@ -49,18 +54,18 @@ class UsersTableSeeder extends Seeder
 //	    	$user->roles()->sync($rep_role);
 //	    });
 //
-//	    $contractor_user = factory("App\User")->create([
-//        	'username'=>'isuru',
-//        	"device_id"=>"",
-//        	'status'=>'verified',
-//	        'email'=>"isuru@twmg.com.au",
-//	        "password"=>bcrypt("twmg#2019")
-//	    ])->each(function($user) use($contractor_role){
-//	    	$user->roles()->sync($contractor_role);
-//	    });
+	    $contractor_user = factory("App\User")->create([
+        	'username'=>'isuru',
+        	"device_id"=>"",
+        	'status'=>'verified',
+	        'email'=>"isuru@twmg.com.au",
+	        "password"=>bcrypt("twmg#2019")
+	    ])->each(function($user) use($contractor_role){
+	    	$user->roles()->sync($contractor_role);
+	    });
 //
-		// $admin_role->first()->users()->sync($admin_user);   
-		// $rep_user->first()->roles()->sync($rep_role); 
-		// $contractor_user->first()->users()->sync($contractor_role);  
+		// $admin_role->first()->users()->sync($admin_user);
+		// $rep_user->first()->roles()->sync($rep_role);
+		// $contractor_user->first()->users()->sync($contractor_role);
 	}
 }
