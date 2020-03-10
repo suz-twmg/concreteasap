40d2c993ccd08c0e4ae38759575848cb510279dc
Agung Kurnia Robbi <robbi@twmg.com.au>
2020-03-10 11:42:41 +0700

    Update order reo requesst

--- app/Http/Controllers/Contractor/Reo/OrderController.php
+++ app/Http/Controllers/Contractor/Reo/OrderController.php
@@ -22,31 +22,13 @@ class OrderController extends Controller
     }
 
     public function create(ContractReoRequest $request){
-        $validator = Validator::make($request->all(), [
-            'suburb' => 'required|max:255',
-            'delivery_date' => 'required',
-            "time_preference1" => 'required',
-            "time_preference2" => 'required',
-            "time_preference3" => 'required',
-            "time_deliveries" => 'required',
-            "urgency" => 'required',
-            "preference" => 'required',
-            "products" => 'required'
-        ]);
-
-        if (!$validator->fails()) {
-            $order=$this->order_reo->createReo($request->all());
-            if (!is_null($order)) {
-                return response()->json(array("message" => "The Job has been placed"), 200);
-            }
-            else{
-                return response()->json(array("message" => "There is an issue while placing a job."), 200);
-            }
-        } else {
-            return response()->json($validator->errors(), 400);
+        $order=$this->order_reo->createReo($request->all());
+        if (!is_null($order)) {
+            return response()->json(array("message" => "The Job has been placed"), 200);
         }
-       
-
+        else{
+            return response()->json(array("message" => "There is an issue while placing a job."), 200);
+        }
     }
 
 }
