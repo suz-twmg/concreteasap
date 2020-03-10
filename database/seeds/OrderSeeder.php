556eb6c1e6675221c46d9f3d8a5152d14bdff671
suz-twmg <sujan@thewebsitemarketinggroup.com.au>
2020-03-10 11:48:06 +1100

    New Changs

--- database/seeds/OrderSeeder.php
+++ database/seeds/OrderSeeder.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Models\Order\Order;
+use App\Models\Order\orderDetail;
 use Illuminate\Database\Seeder;
 
 class OrderSeeder extends Seeder
@@ -12,5 +14,32 @@ class OrderSeeder extends Seeder
     public function run()
     {
         //
+        factory(Order::class)->create([
+            'order_hash_id'=>"",
+            "user_id"=>2,
+            "status"=>"Pending",
+            "order_type"=>"reo",
+            "job-id"=>"02-03-20"
+        ])->save(function(){
+
+        });
+
+        factory(orderDetail::class)->create([
+            'address'=>"50 Delhi Road",
+            "suburb"=>"North Ryde",
+            "post_code"=>2144,
+            "state"=>"NSW",
+            "delivery_date"=>"2020-03-20",
+            "delivery_date1"=>"2020-03-22",
+            "delivery_date2"=>"2020-03-24",
+            "time_preference1"=>"10:00",
+            "time_preference2"=>"12:00",
+            "time_preference3"=>"13:00",
+            "urgency"=>"Immediate",
+            "preference"=>"test",
+            "delivery_instructions"=>"",
+            "special_instructions"=>"",
+            "order_id"=>$order->id
+        ]);
     }
 }
