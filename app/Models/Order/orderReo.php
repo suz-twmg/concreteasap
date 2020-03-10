caf3362c17087289d5eed05116eaa11fa928c64f
Agung Kurnia Robbi <robbi@twmg.com.au>
2020-03-10 11:34:31 +0700

    - Add table name in orderReo Model - Remove return error message in order reo repo

--- app/Models/Order/orderReo.php
+++ app/Models/Order/orderReo.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class orderReo extends Model
 {
+    protected $table = 'order_reo';
+
     public function order()
     {
         return $this->belongsTo(Order::class, 'id', 'order_id');
